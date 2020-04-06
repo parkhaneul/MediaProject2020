3ec3e16479fe960fae66a9e60d4881323a1ec70e
parkhaneul <gksmf077080@gmail.com>
2020-04-06 12:34:44 +0900

    controller update

---
+++ Assets/Scripts/CharacterAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAction : MonoBehaviour
+{
+    public float speed;
+    public void move(Point point)
+    {
+        turn(point);
+        this.gameObject.transform.position += this.gameObject.transform.forward * speed;
+    }
+
+    public void turn(Point point)
+    {
+        var lookAtVector = this.transform.position + new Vector3(point.x, point.y, point.z) * speed;
+        
+        //Debug.Log(lookAtVector);
+        this.gameObject.transform.LookAt(lookAtVector);
+    }
+
+    public void action(bool action)
+    {
+        Debug.Log("action");
+    }
+}
