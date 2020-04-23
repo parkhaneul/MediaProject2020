900a87be08d912a14096b8e4bd706dd187fd1550
parkhaneul <gksmf077080@gmail.com>
2020-04-23 12:46:10 +0900

    Code Readability Upgrade

--- Assets/LoadingBarObject.cs
+++ Assets/LoadingBarObject.cs
@@ -3,23 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UnityEngine.TestTools;
 
 public class LoadingBarObject : MonoBehaviour
 {
     private MissionLogic mLogic;
     private float percent;
-    
+
+    /// <summary>
+    /// basicPivot is (0,0,0) ,(0,0,0) is (-1,-1,-1), (1,1,1) is (1,1,1)
+    /// </summary>
+    public Vector3 pivot = Vector3.zero;
+    private Vector3 initPosition;
+    //(-10,0,0)
+    public float interval = 0.1f;
     public void Start()
     {
+        initPosition = this.transform.position - pivot/2;
+        
         if (mLogic == null)
             mLogic = MissionLogic.Instance;
 
         this.ObserveEveryValueChanged(_ => _.mLogic.getPercent())
             .Subscribe(_ =>
             {
-                this.gameObject.transform.position = Vector3.up * 0.5f + Vector3.right * (-9.5f + _ * 0.05f);
-                this.gameObject.transform.localScale = Vector3.one + Vector3.right * _ * 0.1f;
+                this.gameObject.transform.localScale = Vector3.one + Vector3.right * _ * interval;
+                this.gameObject.transform.position = initPosition + mulitple(Vector3.right/2,gameObject.transform.localScale - Vector3.one);
             })
             .AddTo(this);
+    }
+
+    public Vector3 mulitple(Vector3 a, Vector3 b)
+    {
+        return new Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
     }
 }
