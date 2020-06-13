45d7f25edaf41244b7762c7a11d068587e28cfb6
Chan <cliwo@naver.com>
2020-06-13 23:10:30 +0900

    Add Stun Fx

---
+++ Assets/Scripts/Etc/Fx_Stunned.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fx_Stunned : MonoBehaviour
+{
+    public float timeToDie = 5.0f;
+    private float startTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > startTime + timeToDie)  
+            Destroy(gameObject); 
+    }
+}
